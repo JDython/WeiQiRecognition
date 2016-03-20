6053d0a1295bf3901ef2cc380297916a518e2c9a
JiaoDian <privatedian@aliyun.com>
2016-03-20 16:39:29 +0800

    delete bug code

--- recognize_line.py
+++ recognize_line.py
@@ -1,59 +1,50 @@
 #!/usr/bin/python
+#coding=utf-8
 
+import settings
 import cv2
 import numpy as np
 
-pic_name = 'three_lines.jpg'
-line_num=0
-raw_line_data =[]
-del_line_data = []
-img = cv2.imread(pic_name,cv2.IMREAD_GRAYSCALE)
-res=cv2.resize(img,(640,480),interpolation=cv2.INTER_CUBIC)
-res=cv2.GaussianBlur(res,(5,5),0)
-edges = cv2.Canny(res,50,150)
-lines = cv2.HoughLines(edges,1,np.pi/180,180)
+
+def lineRecognizer(path):
+	'''
+    :param path 带识别图片的路径
+	:returns lines_data 识别出的直线数据；resize_pic 修改后的图片
+	'''
+	img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
+	resize_pic=cv2.resize(img,(640,480),interpolation=cv2.INTER_CUBIC)
+	edges = cv2.Canny(resize_pic,50,150)
+	lines_data = cv2.HoughLines(edges,1,np.pi/180,150)
+	return lines_data,resize_pic
 
 
-for rho,theta in lines[0]:
-	raw_line_data.append((rho,theta))
-	del_line_data.append((rho,theta))
-	print 'rho:  '+str(rho)+'theta:  '+str(theta)
-	line_num +=1
-	a = np.cos(theta)
-	b = np.sin(theta)
-	x0 = a*rho
-	y0 = b*rho
-	x1 = int(x0 + 1000*(-b))
-	y1 = int(y0 + 1000*(a))
-	x2 = int(x0 - 1000*(-b))
-	y2 = int(y0 - 1000*(a))
-	cv2.line(res,(x1,y1),(x2,y2),(0,0,255),2)
-	cv2.imshow('image',res)
-	k = cv2.waitKey(0) & 0xFF
-	if k == 27:
-		cv2.destroyAllWindows()
+def drawLines(lines_data,pic):
+	'''
+	横线rho为正，竖线为负。theta均为0；
+	:param lines_data:
+	:param pic:
+	:return: rho列表
+	'''
+	line_para=[]
+	for rho,theta in lines_data[0]:
+		print 'rho:  '+str(rho)+'theta:  '+str(theta)
+		line_para.append(rho)
+		a = np.cos(theta)
+		b = np.sin(theta)
+		x0 = a*rho
+		y0 = b*rho
+		x1 = int(x0 + 1000*(-b))
+		y1 = int(y0 + 1000*(a))
+		x2 = int(x0 - 1000*(-b))
+		y2 = int(y0 - 1000*(a))
+		cv2.line(pic,(x1,y1),(x2,y2),(255,0,0),2)
+		cv2.imshow('image',pic)
+		k = cv2.waitKey(0) & 0xFF
+		if k == 27:
+			cv2.destroyAllWindows()
+	return line_para
 
-
-IGNORE_ERROR = 15
-line_data =[]
-
-for i in raw_line_data:
-	temp_line_data =[]
-	max_rho_error = i[0]+IGNORE_ERROR
-	min_rho_error = i[0]-IGNORE_ERROR
-	max_theta_error = i[1]+IGNORE_ERROR
-	min_theta_error = i[1]-IGNORE_ERROR
-	for item in del_line_data:
-		if min_rho_error<item[0]<max_rho_error and min_theta_error<item[1]<max_theta_error:
-			temp_line_data.append(del_line_data.pop(del_line_data.index(item)))
-
-	if len(temp_line_data):
-		total_rho =0
-		total_theta =0
-		length = len(temp_line_data)
-		for num in range(length):
-			total_rho +=temp_line_data[num][0]
-			total_theta+=temp_line_data[num][1]
-		line_data.append((total_rho/length,total_theta/length))
-#print line_data
-print 'The number of lines is '+str(len(line_data))
+if __name__ == '__main__':
+	path=settings.RECOG_CONFIGE['pic_path']+settings.RECOG_CONFIGE['pic_name']
+	lines_data,resize_pic = lineRecognizer(path)
+	drawLines(lines_data,resize_pic)
