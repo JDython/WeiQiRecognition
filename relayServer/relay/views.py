d00c0ca6395da575e6c19ce40c80532bb1faf6cd
JiaoDian <privatedian@aliyun.com>
2016-04-03 21:16:34 +0800

    display digitization weiqi

--- relayServer/relay/views.py
+++ relayServer/relay/views.py
@@ -6,8 +6,13 @@ from django.http import JsonResponse
 # Create your views here.
 
 def relay(request):
-    img_path = getattr(config,'DJANGO_STATIC_PATH')
-    last_img_name = getattr(dataHelper,'search_last_img')()
+    # img_path = getattr(config,'DJANGO_STATIC_PATH')
+    # last_img_name = getattr(dataHelper,'search_last_img')()
+    # if request.is_ajax() and request.method == 'GET':
+    #     return JsonResponse(img_path+last_img_name, safe=False)
+    # return render(request,'weiqi.html',{'imgPath':img_path+last_img_name})
+
+    dyadic_array = dataHelper.dyadic_array_generator()
     if request.is_ajax() and request.method == 'GET':
-        return JsonResponse(img_path+last_img_name, safe=False)
-    return render(request,'relay.html',{'imgPath':img_path+last_img_name})
+        return JsonResponse(dyadic_array, safe=False)
+    return render(request,'weiqi.html',{'dyadic_array':dyadic_array})
