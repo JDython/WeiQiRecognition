f36846b6d63b7870927b6bb7d7a6230f36cc10e3
JiaoDian <privatedian@aliyun.com>
2016-04-27 16:22:32 +0800

    chess_composition table

--- relayServer/relay/models.py
+++ relayServer/relay/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class chess_composition(models.Model):
+    id = models.AutoField(primary_key=True)
+    time = models.DateTimeField()
+    matrix =  models.CharField(max_length=1200)
+
+    class Meta:
+        db_table='chess_composition'
+        ordering=['-time']
