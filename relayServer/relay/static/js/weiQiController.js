f91dba1cf43981335e7507d0314686bab507b1be
JiaoDian <privatedian@aliyun.com>
2016-04-30 15:16:20 +0800

    the edge of chessboard bold

--- relayServer/relay/static/js/weiQiController.js
+++ relayServer/relay/static/js/weiQiController.js
@@ -2,8 +2,12 @@
 
 //棋子半径为15px
 weiQiController.r = 14;
+//棋盘线密度
 weiQiController.lineThick =15;
+//棋盘黑点半径大小
 weiQiController.blackPointR = 5;
+//棋盘边框宽度
+weiQiController.lineWidth = 3;
 
 //棋盘参数 单位为px
 weiQiController.chessBoardParm = {
@@ -23,8 +27,13 @@ $(document).ready(function(){
 
 
 //画线
-weiQiController.drawLine = function(ctx,x1,y1,x2,y2){
+weiQiController.drawLine = function(ctx,x1,y1,x2,y2,isEdge){
     ctx.beginPath();
+    if(isEdge){
+        ctx.lineWidth = weiQiController.lineWidth;
+    }else{
+        ctx.lineWidth = 1;
+    }
     ctx.moveTo(x1,y1);
     ctx.lineTo(x2,y2);
     weiQiController.board.stroke();
@@ -69,20 +78,27 @@ weiQiController.drawBoard = function(){
     weiQiController.board.fillRect(weiQiController.chessBoardParm['rectOrigin'][0],
     weiQiController.chessBoardParm['rectOrigin'][1],
     weiQiController.chessBoardParm['width'],weiQiController.chessBoardParm['height']);     //parm : 起始坐标、宽高
-
+    var isEdge;
     for(var i=1;i<20;i++){
         //画横线
+        if(i==1 || i==19){
+            isEdge=true;
+        }else{
+            isEdge=false;
+        }
         weiQiController.drawLine(weiQiController.board,
         weiQiController.chessBoardParm['rectOrigin'][0]+weiQiController.chessBoardParm['margin'],
         2*weiQiController.lineThick*i+weiQiController.chessBoardParm['rectOrigin'][1],
         weiQiController.chessBoardParm['rectOrigin'][0]+19*2*weiQiController.lineThick,
-        2*weiQiController.lineThick*i+weiQiController.chessBoardParm['rectOrigin'][1]);
+        2*weiQiController.lineThick*i+weiQiController.chessBoardParm['rectOrigin'][1],
+        isEdge);
         //画竖线
         weiQiController.drawLine(weiQiController.board,
         weiQiController.chessBoardParm['rectOrigin'][0]+2*weiQiController.lineThick*i,
         2*weiQiController.lineThick+weiQiController.chessBoardParm['rectOrigin'][1],
         weiQiController.chessBoardParm['rectOrigin'][0]+2*weiQiController.lineThick*i,
-        19*2*weiQiController.lineThick+weiQiController.chessBoardParm['rectOrigin'][1]);
+        19*2*weiQiController.lineThick+weiQiController.chessBoardParm['rectOrigin'][1],
+        isEdge);
     }
     for(var j=0;j<3;j++){
         for(var k=0;k<3;k++){
