35bb7abd6c22e152ec8ff62f11bf722e9e6e19fa
JiaoDian <privatedian@aliyun.com>
2016-04-30 12:34:26 +0800

    remove fake data and use generation matrix data

--- relayServer/relay/views.py
+++ relayServer/relay/views.py
@@ -1,18 +1,13 @@
-import config
 import dataHelper
+from models import *
 from django.shortcuts import render
 from django.http import JsonResponse
 
 # Create your views here.
 
 def relay(request):
-    # img_path = getattr(config,'DJANGO_STATIC_PATH')
-    # last_img_name = getattr(dataHelper,'search_last_img')()
-    # if request.is_ajax() and request.method == 'GET':
-    #     return JsonResponse(img_path+last_img_name, safe=False)
-    # return render(request,'weiqi.html',{'imgPath':img_path+last_img_name})
-
-    dyadic_array = dataHelper.dyadic_array_generator()
+    with open('relay/static/chessboard_position.txt','r') as f:
+        matrix = eval(f.readlines()[0])
     if request.is_ajax() and request.method == 'GET':
-        return JsonResponse(dyadic_array, safe=False)
-    return render(request,'weiqi.html',{'dyadic_array':dyadic_array})
+        return JsonResponse(matrix, safe=False)
+    return render(request,'weiqi.html',{'dyadic_array':matrix})
